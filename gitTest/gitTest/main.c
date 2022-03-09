51355110a147a5c894b01aa878a2253b598162ee
ruanshutian <1204970556@qq.com>
2022-03-09 13:49:51 +0800

    First Commit

---
+++ gitTest/gitTest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  gitTest
+//
+//  Created by Aron Ruan on 2022/3/9.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("This is gitTest\n");
+    return 0;
+}
