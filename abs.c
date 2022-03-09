76f4a3a73cffd2394fb1864c4ab1f49fae5e2b9b
ruanshutian <1204970556@qq.com>
2022-03-09 22:38:16 +0800

    oc

---
+++ abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("3*3+5*2 = %d\n", 3 * 3 + 5 * 2);
+
+    printf("11/3 = %d remainder of %d \n", 11 / 3, 11 % 3);
+
+    printf("11/3.0 = %f \n", 11 / (float)3);
+
+    printf("The absolute value of -5 is %d\n", abs(-5));
+
+    system("pause");
+}
---
+++ floatTest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    double y = 12345.6789;
+    printf("y is %f\n", y);
+    printf("y is %e\n", y);
+
+    printf("y is %.2f\n", y);
+    printf("y is %.2e\n", y);
+
+    system("pause");
+}
